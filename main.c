ddb574c023f9590cedf11d58c75659b28a383e64
Colleague Riley <ColleagueRiley@gmail.com>
2023-03-16 18:53:50 -0400

    updated to current

--- main.c
+++ main.c
@@ -10,14 +10,11 @@
 #include "RGFW.h"
 
 int main(){
-    RGFW_window w = RGFW_createWindow("name", 500, 500, 500, 500, NULL);
-
-    if (w.display == NULL)
-        return 0;
+    RGFW_window* w = RGFW_createWindowPointer("name", 500, 500, 500, 500, 0);
 
     while (1){
-        RGFW_checkEvents(&w); 
-        RGFW_clear(&w, 255, 255, 255, 255);
+        RGFW_checkEvents(w); 
+        RGFW_clear(w, 255, 255, 255, 255);
 
         glBegin(GL_POLYGON);
             glColor3f(1, 0, 0); glVertex2f(-0.6, -0.75);
@@ -25,9 +22,9 @@ int main(){
             glColor3f(0, 0, 1); glVertex2f(0, 0.75);
         glEnd();
 
-        if (w.event.type == RGFW_quit)
+        if (w->event.type == RGFW_quit)
             break;
     }
 
-    RGFW_closeWindow(&w);
+    RGFW_closeWindow(w);
 }
