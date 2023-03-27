213a6c951ec28ea0d9f6edcaf33b5569eb7e2f79
Colleague Riley <50218871+ColleagueRiley@users.noreply.github.com>
2023-03-27 10:59:12 -0400

    added a multi-window support example

--- main.c
+++ main.c
@@ -13,36 +13,42 @@
 unsigned char icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF,    0xFF, 0x00, 0x00, 0xFF,     0xFF, 0x00, 0x00, 0xFF,   0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF,     0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
 
 int main(){
-    RGFW_window* w = RGFW_createWindowPointer("name", 500, 500, 500, 500, RGFW_ALLOW_DND);
+    RGFW_window* wins[2] = {
+                            RGFW_createWindowPointer("name", 500, 500, 500, 500, RGFW_ALLOW_DND),
+                            RGFW_createWindowPointer("subwindow", 200, 200, 200, 200, RGFW_ALLOW_DND)
+                           };
 
-    RGFW_setIcon(w, icon, 3, 3, 4);
-    w->fpsCap = 60;
+    RGFW_setIcon(wins[0], icon, 3, 3, 4);
+    wins[0]->fpsCap = 60;
 
     while (1){
-        RGFW_checkEvents(w); 
-        RGFW_clear(w, 255, 255, 255, 255);
+        for (int i = 0; i < 2; i++){
+            RGFW_checkEvents(wins[i]); 
+            RGFW_clear(wins[i], 255, 255, 255, 255);
 
-        glBegin(GL_POLYGON);
-            glColor3f(1, 0, 0); glVertex2f(-0.6, -0.75);
-            glColor3f(0, 1, 0); glVertex2f(0.6, -0.75);
-            glColor3f(0, 0, 1); glVertex2f(0, 0.75);
-        glEnd();
+            glBegin(GL_POLYGON);
+                glColor3f(1, 0, 0); glVertex2f(-0.6, -0.75);
+                glColor3f(0, 1, 0); glVertex2f(0.6, -0.75);
+                glColor3f(0, 0, 1); glVertex2f(0, 0.75);
+            glEnd();
+        }
 
         if (w->event.type == RGFW_quit)
             break;
 
-        if (RGFW_isPressedS(w, "Up"))
-            printf("Pasted : %s\n", RGFW_readClipboard(w));
-        else if (RGFW_isPressedS(w, "Down"))
-            RGFW_writeClipboard(w, "DOWN");
-        else if (RGFW_isPressedS(w, "Space"))
-            printf("fps : %i\n", w->fps);   
+        if (RGFW_isPressedS(wins[0], "Up"))
+            printf("Pasted : %s\n", RGFW_readClipboard(wins[0]));
+        else if (RGFW_isPressedS(wins[0], "Down"))
+            RGFW_writeClipboard(wins[0], "DOWN");
+        else if (RGFW_isPressedS(wins[0], "Space"))
+            printf("fps : %i\n", wins[0]->fps);   
 
 
         int i;
         for (i = 0; i < w->event.droppedFilesCount; i++)
-            printf("dropped : %s\n", w->event.droppedFiles[i]);
+            printf("dropped : %s\n", wins[0]->event.droppedFiles[i]);
     }
-
-    RGFW_closeWindow(w);
+    
+    for (int i = 0; i < 2; i++)  
+        RGFW_closeWindow(wins[i]);
 }
