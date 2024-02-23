d0a433c390469fdadb1b9bdaa7a35a55919eff34
ColleagueRiley <ColleagueRiley@gmail.com>
2024-02-23 16:47:44 -0500

    added license name

--- RGFW.h
+++ RGFW.h
@@ -1,6 +1,8 @@
 /*
 * Copyright (C) 2023 ColleagueRiley
 *
+* libpng license
+*
 * This software is provided 'as-is', without any express or implied
 * warranty.  In no event will the authors be held liable for any damages
 * arising from the use of this software.
