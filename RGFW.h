5148c6fe72c73a251083c2a9b534f28b87601226
ColleagueRiley <ColleagueRiley@gmail.com>
2024-03-09 19:24:00 -0500

    fix view typo

--- RGFW.h
+++ RGFW.h
@@ -4499,8 +4499,8 @@ RGFW_window* RGFW_createWindow(const char* name, RGFW_rect rect, u16 args) {
 
 
 	NSWindow_contentView_setWantsLayer(win->src.window, true);
-	NSWindow_setContentView(win, (NSView*)view);
-	
+	NSWindow_setContentView(win, (NSView*)win->src.view);
+
 	win->src.rSurf = NSOpenGLView_openGLContext(win->src.view);
 	NSOpenGLContext_makeCurrentContext(win->src.rSurf);
 
