cee9d3842e432e88ac4f41289c3f1dabcef01aa5
Colleague Riley <ColleagueRiley@gmail.com>
2024-03-16 12:55:01 -0400

    update for RGFW update + macos multi-window support

--- examples/basic/main.c
+++ examples/basic/main.c
@@ -13,29 +13,36 @@ void* loop2(void *);
 unsigned char icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
 unsigned char running = 1, running2 = 1;
 
+RGFW_window* win2;
+
 int main() {
     RGFW_window* win = RGFW_createWindow("RGFW Example Window", RGFW_RECT(500, 500, 500, 500), RGFW_ALLOW_DND | RGFW_CENTER);
     RGFW_window_makeCurrent(win);
-    
-    if (win == NULL)
-        return 1;
-    
+
+    RGFW_window_setIcon(win, icon, RGFW_AREA(3, 3), 4);
+
+    RGFW_window_swapInterval(win, 1);
+
+    #ifdef RGFW_MACOS
+    win2 = RGFW_createWindow("subwindow", RGFW_RECT(200, 200, 200, 200), 0);
+    #endif
+
     RGFW_createThread(loop2, NULL); /* the function must be run after the window of this thread is made for some reason (using X11) */
 
     unsigned char i;
-
-    RGFW_window_setIcon(win, icon, RGFW_AREA(3, 3), 4);
 
     #ifndef RGFW_VULKAN
     glEnable(GL_BLEND);
     #endif
 
-    RGFW_window_swapInterval(win, 1);
-
     glEnable(GL_BLEND);             
     glClearColor(0, 0, 0, 0);
 
-    while (running && !RGFW_isPressedI(win, RGFW_Escape)) {
+    while (running && !RGFW_isPressedI(win, RGFW_Escape)) {   
+        #ifdef __APPLE__
+        RGFW_window_checkEvent(win2);
+        #endif
+
         while (RGFW_window_checkEvent(win)) {
             if (win->event.type == RGFW_windowAttribsChange) {
                 printf("attribs changed\n");
@@ -71,16 +78,18 @@ int main() {
             else if (win->event.type == RGFW_jsAxisMove && !win->event.button)
                 printf("{%i, %i}\n", win->event.axis[0].x, win->event.axis[0].y);
         }
-        
-        RGFW_window_makeCurrent(win);
+
         drawLoop(win);
     }
 
     running2 = 0;
+    
     RGFW_window_close(win);
 }
 
 void drawLoop(RGFW_window *w) {
+    RGFW_window_makeCurrent(w);
+
     #ifndef RGFW_VULKAN
     glClearColor(255, 255, 255, 255);
 
@@ -100,15 +109,21 @@ void drawLoop(RGFW_window *w) {
     RGFW_window_swapBuffers(w); /* NOTE(EimaMei): Rendering should always go: 1. Clear everything 2. Render 3. Swap buffers. Based on https://www.khronos.org/opengl/wiki/Common_Mistakes#Swap_Buffers */
 }
 
+
 void* loop2(void* args) {
     #ifndef __APPLE__
     RGFW_window* win = RGFW_createWindow("subwindow", RGFW_RECT(200, 200, 200, 200), 0);
+    #else
+    RGFW_window* win = win2;
+    #endif
 
     while (running2) {
         /* 
             not using a while loop here because there is only one event I care about 
         */
+        #ifndef __APPLE__
         RGFW_window_checkEvent(win);
+        #endif
 
         /* 
             I could've also done
@@ -124,9 +139,6 @@ void* loop2(void* args) {
 
     running = 0;
     RGFW_window_close(win);
-    #else
-    printf("Managing windows on a seperate thread is not supported on MacOS :(\n");
-    #endif
 
     return NULL;
 }
