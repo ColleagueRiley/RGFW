0755aa5372744e53e92d32f8439ea8f129bd20ee
Colleague Riley <colleagueRiley@gmail.com>
2024-08-04 23:09:45 -0400

    rename examples

---
+++ examples/events/events.c
@@ -0,0 +1,74 @@
+#define RGFW_IMPLEMENTATION
+#include "RGFW.h"
+
+int main(void) {
+    RGFW_window* win = RGFW_createWindow("RGFW Events", RGFW_RECT(500, 500, 500, 500), (u64)RGFW_CENTER | RGFW_ALLOW_DND | RGFW_TRANSPARENT_WINDOW);
+
+    while (RGFW_window_shouldClose(win) == 0) {
+        RGFW_window_eventWait(win, RGFW_NEXT);
+        while (RGFW_window_checkEvent(win)) {
+            switch (win->event.type) {
+                case RGFW_keyPressed:
+                    printf("Key pressed\n");
+                    break;
+                case RGFW_keyReleased:
+                    printf("Key released\n");
+                    break;
+                case RGFW_mouseButtonPressed:
+                    printf("mouse button pressed\n");
+                    break;
+                case RGFW_mouseButtonReleased:
+                    printf("Mouse Button Released\n");
+                    break;
+                case RGFW_mousePosChanged:
+                    if (RGFW_isPressed(win, RGFW_ControlL))
+                        printf("Mouse pos changed %i %i\n", win->event.point.x, win->event.point.y);
+                    break;
+                case RGFW_windowMoved:
+                    printf("window moved %i %i\n", win->r.x, win->r.y);
+                    break;
+                case RGFW_windowResized:
+                    printf("window resize %i %i\n", win->r.w, win->r.h);
+                    break;
+                case RGFW_focusIn:
+                    printf("Focused\n");
+                    break;
+                case RGFW_focusOut:
+                    printf("Unfocused\n");
+                    break;
+                case RGFW_mouseEnter:
+                    printf("Mouse Entered %i %i\n", win->event.point.x, win->event.point.y);
+                    break;
+                case RGFW_mouseLeave:
+                    printf("Mouse left\n");
+                    break;
+                case RGFW_windowRefresh:
+                    printf("Refresh\n");
+                    break;
+                case RGFW_quit:
+                    printf("Quit\n");
+                    break;
+                case RGFW_dnd: {
+                    printf("DND Drop : %i %i\n", win->event.point.x, win->event.point.y);
+                    u32 i;
+                    for (i = 0; i < win->event.droppedFilesCount; i++)
+                        printf("dropped : %s\n", win->event.droppedFiles[i]);
+                    break;
+                }
+                case RGFW_dnd_init:
+                    printf("DND Init : %i %i\n", win->event.point.x, win->event.point.y);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        glClearColor(0.25, 0, 0.15, 0.25);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        RGFW_window_swapBuffers(win);
+    }
+
+    RGFW_window_close(win);
+}
+
---
+++ examples/gl33/gl33.c
@@ -0,0 +1,204 @@
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
+#ifndef __EMSCRIPTEN__
+#define RGL_LOAD_IMPLEMENTATION
+#include "rglLoad.h"
+#else
+#include <GLES3/gl3.h>
+#endif
+
+#define RGFW_ALLOC_DROPFILES
+#define RGFW_IMPLEMENTATION
+#define RGFW_PRINT_ERRORS
+#include <RGFW.h>
+
+#define MULTILINE_STR(...) #__VA_ARGS__
+
+#include <stdbool.h>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+#ifndef __EMSCRIPTEN__
+const char *vertexShaderSource = MULTILINE_STR(
+
+\x23version 330 core\n
+layout (location = 0) in vec3 aPos;
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+);
+
+    ;
+const char *fragmentShaderSource = MULTILINE_STR(
+\x23version 330 core\n
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+);
+#else
+   const char *vertexShaderSource = MULTILINE_STR(
+      attribute vec3 aPos;  
+      void main() {
+         gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+      }
+   );
+
+  const char *fragmentShaderSource = MULTILINE_STR(
+      void main() {
+        gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+      }
+    );
+#endif
+
+
+int main(void)
+{
+    RGFW_setGLVersion(RGFW_GL_CORE, 3, 3);
+    RGFW_window* window = RGFW_createWindow("LearnOpenGL", RGFW_RECT(SCR_WIDTH, SCR_HEIGHT, SCR_WIDTH, SCR_HEIGHT), RGFW_ALLOW_DND | RGFW_CENTER);
+    
+    if (window == NULL)
+    {
+        printf("Failed to create RGFW window\n");
+        return -1;
+    }
+    RGFW_window_makeCurrent(window);
+    // RGFW_window_swapInterval(window, 60);
+
+    #ifndef RGFW_WEBASM
+    if (RGL_loadGL3((RGLloadfunc)RGFW_getProcAddress))
+    {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+    #endif
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    u32 fps = 0;
+
+    // render loop
+    // -----------
+    bool running = true;
+    while (running && !RGFW_isPressed(window, RGFW_Escape))
+    {
+        while (RGFW_window_checkEvent(window)) {
+            if (window->event.type == RGFW_quit) {
+                running = false;
+                break;
+            }
+        }
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time
+
+        if (RGFW_isPressed(window, RGFW_Space))
+            printf("fps : %i\n", fps);
+                        
+        RGFW_window_swapBuffers(window);
+        fps = RGFW_window_checkFPS(window, 60);
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    RGFW_window_close(window);
+    return 0;
+}
