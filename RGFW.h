cbb201409c9d65a2b44bb412a96b4645820c41c0
ColleagueRiley <ColleagueRiley@users.noreply.github.com>
2024-12-28 20:12:50 +0000

    deploy: 6efe2a9283937e82822d18687341421a5787c789

--- RGFW.h
+++ RGFW.h
@@ -7335,10 +7335,7 @@ RGFW_UNUSED(win); /*!< if buffer rendering is not being used */
 			case kHIDPage_GenericDesktop: {
 				CFIndex logicalMin = IOHIDElementGetLogicalMin(element);
 				CFIndex logicalMax = IOHIDElementGetLogicalMax(element);
-				
 				if (logicalMax <= logicalMin) return;
-				if (intValue < logicalMin) intValue = logicalMin;
-				if (intValue > logicalMax) intValue = logicalMax;
 
 				//i8 value8 = (i8)intValue;
 				i8 value = (i8)(-100.0 + ((intValue - logicalMin) * 200.0) / (logicalMax - logicalMin));
