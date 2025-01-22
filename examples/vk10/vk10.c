02750f2b4c375dc61d10e7c431b2295fdc52121b
ColleagueRiley <ColleagueRiley@users.noreply.github.com>
2025-01-22 01:14:43 +0000

    deploy: 70b7950332d128cee25d17a273d3e18a6a98d6be

--- examples/vk10/vk10.c
+++ examples/vk10/vk10.c
@@ -111,15 +111,15 @@ int createGraphicsPipeline(RGFW_window* win) {
   VkViewport viewport;
   viewport.x = 0.0f;
   viewport.y = 0.0f;
-  viewport.width = (float)500;
-  viewport.height = (float)500;
+  viewport.width = (float)win->r.w;
+  viewport.height = (float)win->r.h;
   viewport.minDepth = 0.0f;
   viewport.maxDepth = 1.0f;
 
   VkRect2D scissor;
   scissor.offset.x = 0;
   scissor.offset.y = 0;
-  scissor.extent = (VkExtent2D){500, 500};
+  scissor.extent = (VkExtent2D){win->r.w, win->r.h};
 
   VkPipelineViewportStateCreateInfo viewport_state = {0};
   viewport_state.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
@@ -226,7 +226,7 @@ int commandBuffers(RGFW_window* win, RGFW_window_vulkanInfo *vulkWin) {
     render_pass_info.framebuffer = vulkan_info->framebuffers[i];
     render_pass_info.renderArea.offset.x = 0;
     render_pass_info.renderArea.offset.y = 0;
-    render_pass_info.renderArea.extent = (VkExtent2D){500, 500};
+    render_pass_info.renderArea.extent = (VkExtent2D){win->r.w, win->r.h};
 
     VkClearValue clearColor;
     clearColor.color.float32[0] = 1.0f;
@@ -239,15 +239,15 @@ int commandBuffers(RGFW_window* win, RGFW_window_vulkanInfo *vulkWin) {
     VkViewport viewport;
     viewport.x = 0.0f;
     viewport.y = 0.0f;
-    viewport.width = (float)500;
-    viewport.height = (float)500;
+    viewport.width = (float)win->r.w;
+    viewport.height = (float)win->r.h;
     viewport.minDepth = 0.0f;
     viewport.maxDepth = 1.0f;
 
     VkRect2D scissor;
     scissor.offset.x = 0;
     scissor.offset.y = 0;
-    scissor.extent = (VkExtent2D){500, 500};
+    scissor.extent = (VkExtent2D){win->r.w, win->r.h};
 
     vkCmdSetViewport(vulkan_info->command_buffers[i], 0, 1, &viewport);
     vkCmdSetScissor(vulkan_info->command_buffers[i], 0, 1, &scissor);
