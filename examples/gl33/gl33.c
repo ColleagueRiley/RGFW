15cd45f506250b76941c0463784488b0915678be
ColleagueRiley <ColleagueRiley@gmail.com>
2025-02-05 16:53:48 -0500

    remove viewport

--- examples/gl33/gl33.c
+++ examples/gl33/gl33.c
@@ -177,10 +177,9 @@ int main(void)
                 break;
             }
         }
-
+        
         // render
         // ------
-        glViewport(0, 0, window->r.w, window->r.h);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
