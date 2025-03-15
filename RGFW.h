0ba149264cfc86ffde21a5f07b70d78ad1662108
ColleagueRiley <ColleagueRiley@gmail.com>
2025-03-15 17:42:48 -0400

    macos c++

--- RGFW.h
+++ RGFW.h
@@ -544,13 +544,13 @@ typedef RGFW_ENUM(u8, RGFW_gamepadCodes) {
 #endif
 
 #ifdef __cplusplus
-#define RGFW_POINT(x, y) {(i32)(x), (i32)(y)}
-#define RGFW_RECT(x, y, w, h) {(i32)(x), (i32)(y), (i32)(w), (i32)(h)}
-#define RGFW_AREA(w, h) {(u32)(w), (u32)(h)}
+#define RGFW_POINT(x, y) {(i32)x, (i32)y}
+#define RGFW_RECT(x, y, w, h) {(i32)x, (i32)y, (i32)w, (i32)h}
+#define RGFW_AREA(w, h) {(u32)w, (u32)h}
 #else
-#define RGFW_POINT(x, y) (RGFW_point){(i32)(x), (i32)y}
-#define RGFW_RECT(x, y, w, h) (RGFW_rect){(i32)x, (i32)y, (i32)w, (i32)(h)}
-#define RGFW_AREA(w, h) (RGFW_area){(u32)w, (u32)h}
+#define RGFW_POINT(x, y) (RGFW_point){(i32)(x), (i32)(y)}
+#define RGFW_RECT(x, y, w, h) (RGFW_rect){(i32)(x), (i32)(y), (i32)(w), (i32)(h)}
+#define RGFW_AREA(w, h) (RGFW_area){(u32)(w), (u32)(h)}
 #endif
 
 #ifndef RGFW_NO_MONITOR
