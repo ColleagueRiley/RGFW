400931b4eb340dc2a15aa1f28a3de8c70944c4f4
ColleagueRiley <ColleagueRiley@gmail.com>
2025-05-02 22:34:42 -0400

    update

--- RGFW.h
+++ RGFW.h
@@ -5734,14 +5734,28 @@ void RGFW_window_swapInterval(RGFW_window* win, i32 swapInterval) {
 	#if defined(RGFW_OPENGL)
 	// cached pfn to avoid calling glXGetProcAddress more than once
 	static PFNGLXSWAPINTERVALEXTPROC pfn = (PFNGLXSWAPINTERVALEXTPROC)123;
+    static int (*pfn2)(int);
 
 	if (pfn == (PFNGLXSWAPINTERVALEXTPROC)123) {
 		pfn = ((PFNGLXSWAPINTERVALEXTPROC)glXGetProcAddress((GLubyte*) "glXSwapIntervalEXT"));
-		if (pfn == NULL)
+		if (pfn == NULL) {
+            char* array[] = {"GLX_MESA_swap_control", "GLX_SGI_swap_control"};
+            u32 i;
+            for (i = 0; i < sizeof(array) / sizeof(char*); i++)
+    		    pfn2 = ((int(*)(int))glXGetProcAddress((GLubyte*) array[i]));
+
+            if (pfn2 != NULL) {
+        		RGFW_sendDebugInfo(RGFW_typeError, RGFW_errOpenglContext, RGFW_DEBUG_CTX(_RGFW.root, 0), "Failed to load swap interval function, fallingback to the native swapinterval function");
+            } else {
         		RGFW_sendDebugInfo(RGFW_typeError, RGFW_errOpenglContext, RGFW_DEBUG_CTX(_RGFW.root, 0), "Failed to load swap interval function");
-	}
+            }
+        }
+    }
 	if (pfn != NULL)
 		pfn(win->src.display, win->src.window, swapInterval);
+    else if (pfn2 != NULL) {
+        pfn2(swapInterval);
+    }
 	#else
 	RGFW_UNUSED(swapInterval);
 	#endif
