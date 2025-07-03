90a357752b812130d20f500f4cab2efc04164927
EimaMei <chinsman.discord@gmail.com>
2025-07-03 19:59:13 +0300

    Improving event queue system

--- RGFW.h
+++ RGFW.h
@@ -1099,7 +1099,8 @@ typedef RGFW_ENUM(u8, RGFW_errorCode) {
 	RGFW_errIOKit,
 	RGFW_errClipboard,
 	RGFW_errFailedFuncLoad,
-	RGFW_errBuffer, 
+	RGFW_errBuffer,
+	RGFW_errEventQueue,
 	RGFW_infoMonitor, RGFW_infoWindow, RGFW_infoBuffer, RGFW_infoGlobal, RGFW_infoOpenGL, 
 	RGFW_warningWayland, RGFW_warningOpenGL
 };
@@ -1527,7 +1528,6 @@ typedef struct RGFW_info {
     RGFW_window* current;
     i32 windowCount;
     i32 eventLen;
-    i32 eventIndex;
 
     RGFW_mouse* hiddenMouse;
     RGFW_event events[RGFW_MAX_EVENTS];
@@ -2018,7 +2018,6 @@ i32 RGFW_init_ptr(RGFW_info* info) {
     _RGFW->current = NULL; 
     _RGFW->windowCount = -1; 
     _RGFW->eventLen = 0;
-    _RGFW->eventIndex = 0;
     _RGFW->windowCount = 0;
     
     RGFW_MEMSET(_RGFW, 0, sizeof(RGFW_info));
@@ -2044,23 +2043,26 @@ void RGFW_deinit_ptr(RGFW_info* info) {
 
 
 void RGFW_eventQueuePush(RGFW_event event) {
-	if (_RGFW->eventLen >= RGFW_MAX_EVENTS) return;
-	_RGFW->events[_RGFW->eventLen] = event;
-    _RGFW->eventLen++;
+	assert(_RGFW->eventLen >= 0);
+
+	if (_RGFW->eventLen >= RGFW_MAX_EVENTS) {
+		RGFW_sendDebugInfo(RGFW_typeError, RGFW_errEventQueue, RGFW_DEBUG_CTX(NULL, 0), "Event queue limit 'RGFW_MAX_EVENTS' has been reaeched.");
+		return;
+	}
+
+	_RGFW->eventLen += 1;
+	_RGFW->events[RGFW_MAX_EVENTS - _RGFW->eventLen] = event;
 }
 
 RGFW_event* RGFW_eventQueuePop(RGFW_window* win) {
-	RGFW_event* ev; 
-    if (_RGFW->eventLen == 0) return NULL;
+	assert(_RGFW->eventLen >= 0 && _RGFW->eventLen <= RGFW_MAX_EVENTS);
 
-	ev = (RGFW_event*)&_RGFW->events[_RGFW->eventIndex];
+    if (_RGFW->eventLen == 0) {
+		return NULL;
+	}
 
-    _RGFW->eventLen--;
-    if (_RGFW->eventLen >= 0 && _RGFW->eventIndex < _RGFW->eventLen) {
-		_RGFW->eventIndex++;
-    } else if (_RGFW->eventLen == 0) {
-        _RGFW->eventIndex = 0;
-    }
+	RGFW_event* ev = &_RGFW->events[RGFW_MAX_EVENTS - _RGFW->eventLen];
+	_RGFW->eventLen -= 1;
 
 	if (ev->_win != win && ev->_win != NULL) {
         RGFW_eventQueuePush(*ev);
