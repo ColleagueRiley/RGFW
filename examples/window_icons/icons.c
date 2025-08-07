4c53962df6618c1ebba47fa3f8c54690184da04d
ColleagueRiley <colleagueRiley@gmail.com>
2025-08-07 15:48:37 -0400

    split window and mouse icon example

--- examples/window_icons/icons.c
+++ examples/window_icons/icons.c
@@ -12,20 +12,14 @@ int main(void) {
     RGFW_window* win = RGFW_createWindow("RGFW icons", 0, 0, 600, 400, RGFW_windowCenter | RGFW_windowNoResize);
 
     unsigned char* icon = stbi_load("logo.png", &w, &h, &c, 3);
-    RGFW_mouse* mouse = RGFW_loadMouse(icon, w, h, RGFW_formatRGB8);
-
     RGFW_window_setIconEx(win, base_icon, 3, 3, RGFW_formatRGB8, RGFW_iconWindow);
     RGFW_window_setIconEx(win, icon, w, h, RGFW_formatRGB8, RGFW_iconTaskbar);
 
-    RGFW_window_setMouse(win, mouse);
-
-    RGFW_event event;
 
     while (RGFW_window_shouldClose(win) == RGFW_FALSE) {
-        while (RGFW_window_checkEvent(win, &event) && event.type != RGFW_quit);
+        RGFW_pollEvents();
     }
 
-    RGFW_freeMouse(mouse);
     RGFW_window_close(win);
     stbi_image_free(icon);
     return 0;
