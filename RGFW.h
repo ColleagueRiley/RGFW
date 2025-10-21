8b97b015eec2faa1854efe09f66c271c17e88a28
ColleagueRiley <ColleagueRiley@users.noreply.github.com>
2025-10-21 01:26:34 +0000

    deploy: 4a6d4fce6c71c6d4c6629926264d178031e7e82d

--- RGFW.h
+++ RGFW.h
@@ -8628,6 +8628,22 @@ WGPUSurface RGFW_FUNC(RGFW_window_createSurface_WebGPU) (RGFW_window* window, WG
 #define OEMRESOURCE
 #include <windows.h>
 
+#ifndef OCR_NORMAL
+#define OCR_NORMAL 32512
+#define OCR_IBEAM 32513
+#define OCR_WAIT 32514
+#define OCR_CROSS 32515
+#define OCR_UP 32516
+#define OCR_SIZENWSE 32642
+#define OCR_SIZENESW 32643
+#define OCR_SIZEWE 32644
+#define OCR_SIZENS 32645
+#define OCR_SIZEALL 32646
+#define OCR_NO 32648
+#define OCR_HAND 32649
+#define OCR_APPSTARTING 32650
+#endif
+
 #include <windowsx.h>
 #include <shellapi.h>
 #include <shellscalingapi.h>
