7623864ac3b50e3a84ce130c350e600a81d7773d
ColleagueRiley <ColleagueRiley@gmail.com>
2026-01-22 00:55:59 -0500

    update test

--- tests/loop.c
+++ tests/loop.c
@@ -2,13 +2,9 @@
 #include "RGFW.h"
 
 int main(void) {
-	/*
-	 * Note: On OSX, interacting with a window before it will close when windows are being opened/closed rapdily, like in this example
-	 *		 may result in a crash. However I doubt anyone will actually use the API this way.
-	 *
-	 * */
+	RGFW_init();
 	for (size_t i = 0; i < 5; i++) {
-		RGFW_window* win = RGFW_createWindow("name", 100, 100, 500, 500, (u64)0);
+		RGFW_window* win = RGFW_createWindow("name", 100, 100, 500, 500, (u64)RGFW_noDeinitOnClose );
 
 		RGFW_pollEvents();
 		#ifdef RGFW_MACOS
@@ -21,4 +17,5 @@ int main(void) {
 			usleep(50000);
 		#endif
 	}
+	RGFW_deinit();
 }
