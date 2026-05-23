ce466237131babfd7cdf23f327d0cd7930adeb9e
ColleagueRiley <colleagueRiley@gmail.com>
2026-05-22 21:43:08 -0400

    make the clipboard test validate more results

--- tests/clipboard.c
+++ tests/clipboard.c
@@ -12,22 +12,15 @@ int main(void) {
 	data.data = "string";
 	data.length = sizeof("string") - 1;
 	data.type = RGFW_dataText;
-	RGFW_writeClipboard(&data);
+	assert(RGFW_writeClipboard(&data) == RGFW_TRUE);
+
 
 	const RGFW_dataTransfer* clipboard = RGFW_readClipboard();
-	if (clipboard) {
-		printf("clipboard copy %i: '", (u32)clipboard->length);
-		fwrite(clipboard->data, 1, clipboard->length, stdout);
-		printf("\'\n");
+	assert(clipboard);
 
-		if (clipboard->data[clipboard->length - 1] != '\0') {
-			printf("clipboard does not have a null terminator\n");
-			return -1;
-		}
-	} else {
-		printf("failed to read clipboard\n");
-		return -1;
-	}
+	assert(strncmp(clipboard->data, data.data, data.length) == 0);
+
+	assert(clipboard->data[clipboard->length - 1] == '\0');
 
 	RGFW_free(0);
 
