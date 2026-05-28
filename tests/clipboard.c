247f6ced19aa6a5bccf00c966a2677e1373060f3
ColleagueRiley <ColleagueRiley@gmail.com>
2026-05-28 05:06:19 -0400

    revert custom function

--- tests/clipboard.c
+++ tests/clipboard.c
@@ -4,23 +4,15 @@
 #define RGFW_IMPLEMENTATION
 #include "RGFW.h"
 
-RGFW_bool string_compare(const char* str, const char* str2, size_t count);
-RGFW_bool string_compare(const char* str, const char* str2, size_t count) {
-	size_t i = 0;
-	for (i = 0; i < count && str[i] && str2[i] && str[i] == str2[i]; i++);
-	return (i == (count - 1)) ? RGFW_TRUE : RGFW_FALSE;
-}
-
 int main(void) {
 	RGFW_init();
 
 
 	RGFW_dataTransfer data;
-	data.data = "string";
-	data.length = sizeof("string") - 1;
+	data.data = "string\0";
+	data.length = sizeof("string\0") - 1;
 	data.type = RGFW_dataText;
 	assert(RGFW_writeClipboard(&data) == RGFW_TRUE);
-
 
 	const RGFW_dataTransfer* clipboard = RGFW_readClipboard();
 	assert(clipboard);
@@ -29,7 +21,7 @@ int main(void) {
 	printf(" : ");
 	fwrite(clipboard->data, 1, clipboard->length, stdout);
 	printf("\n");
-	assert(string_compare(clipboard->data, data.data, data.length) == RGFW_TRUE);
+	assert(strncmp(clipboard->data, data.data, data.length) == 0);
 
 	assert(clipboard->data[clipboard->length - 1] == '\0');
 
