6d8f34a66da3c847f9da9b2ba5935ce11099ce48
ColleagueRiley <ColleagueRiley@gmail.com>
2026-05-28 04:40:19 -0400

    use a custom function

--- tests/clipboard.c
+++ tests/clipboard.c
@@ -4,6 +4,13 @@
 #define RGFW_IMPLEMENTATION
 #include "RGFW.h"
 
+int string_compare(const char* str, const char* str2, size_t count);
+int string_compare(const char* str, const char* str2, size_t count) {
+	size_t i = 0;
+	for (i = 0; i < count && str[i] && str2[i] && str[i] == str2[i]; i++);
+	return (i == (count - 1));
+}
+
 int main(void) {
 	RGFW_init();
 
@@ -18,7 +25,7 @@ int main(void) {
 	const RGFW_dataTransfer* clipboard = RGFW_readClipboard();
 	assert(clipboard);
 
-	assert(strncmp(clipboard->data, data.data, data.length) == 0);
+	assert(string_compare(clipboard->data, data.data, data.length) != 0);
 
 	assert(clipboard->data[clipboard->length - 1] == '\0');
 
