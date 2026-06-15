d9488ef28d4e74d8908559ecc6f3b1d80b7b218b
ColleagueRiley <colleagueRiley@gmail.com>
2026-06-15 09:59:27 -0400

    add memory leak test

---
+++ tests/memory.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+
+void* myAlloc(size_t size, unsigned int line, const char* file);
+void myFree(void* ptr, unsigned int line, const char* file);
+
+#define RGFW_ALLOC(size) myAlloc(size, __LINE__, __FILE__)
+#define RGFW_FREE(size) myFree(size, __LINE__, __FILE__)
+
+#define GL_SILENCE_DEPRECATION
+#define RGFW_DEBUG
+#define RGFW_IMPLEMENTATION
+#include "RGFW.h"
+
+size_t counter = 0;
+
+void* myAlloc(size_t size, unsigned int line, const char* file) {
+    void* ptr = malloc(size);
+    printf("%s:%i allocated %u bytes at %p\n",  file, line, (unsigned int)size, ptr);
+    counter++;
+
+    return ptr;
+}
+
+void myFree(void* ptr, unsigned int line, const char* file) {
+    counter--;
+    printf("%s:%i freed address %p\n", file, line, ptr);
+    free(ptr);
+}
+
+int main(void) {
+	RGFW_window *win = RGFW_createWindow("RGFW Example Window", 500, 500, 500, 500, RGFW_windowCenter | RGFW_windowAllowDND);
+
+	RGFW_pollEvents();
+
+	if (win) RGFW_window_close(win);
+
+    RGFW_ASSERT(counter == 0 && "MEMORY LEAK!\n");
+    return 0;
+}
+
